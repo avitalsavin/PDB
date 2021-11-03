3bdd4329d211b746c25ca4690ed4a741b55cf7ff
avitalsavin <as15023@nyu.edu>
2021-11-03 19:25:06 -0400

    secondary commit

---
+++ test_pdb.py
@@ -0,0 +1,16 @@
+# test.py
+def func1(a, b):
+    for i in range(1,100):
+
+        b = b - 1
+
+        print("iteration {} : {}\n".format(i, i/a))
+        a = a - b
+
+def func2(a):
+    c = 5
+    func1(a, c)
+
+a = 10
+
+func2(a)
